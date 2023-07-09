14786b49e02cd1c85f78586544aa021a67262dfe
caregor <caregor@gmail.com>
2023-07-09 15:03:06 +0500

    Task 2 is done.

--- task2.py
+++ task2.py
@@ -3,3 +3,14 @@
 Дан список повторяющихся элементов. Вернуть список с дублирующимися элементами. В результирующем списке не должно
 быть дубликатов.
 """
+
+
+def remove_duplicates(input_list):
+    unique_set = set(input_list)
+    unique_list = list(unique_set)
+    return unique_list
+
+
+input_list = [1, 2, 2, 3, 4, 4, 5, 6, 6, 6, 7, 8, 9]
+result_list = remove_duplicates(input_list)
+print(result_list)
